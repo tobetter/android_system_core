a6fcb3463409c93410646b20bf5652b9fb0c4a9c
Zhi Zhou <zhi.zhou@amlogic.com>
2014-09-04 10:11:59 -0400

    audio: add DTS & TRUEHD support
    Change-Id: I1d3294b6e874fc898bdced3dbad17d6ec10ee9cd

--- include/system/audio.h
+++ include/system/audio.h
@@ -251,6 +251,8 @@ typedef enum {
     AUDIO_FORMAT_OPUS                = 0x08000000UL,
     AUDIO_FORMAT_AC3                 = 0x09000000UL,
     AUDIO_FORMAT_E_AC3               = 0x0A000000UL,
+    AUDIO_FORMAT_TRUEHD              = 0x0B000000UL,
+    AUDIO_FORMAT_DTS                 = 0x0C000000UL,
     AUDIO_FORMAT_MAIN_MASK           = 0xFF000000UL,
     AUDIO_FORMAT_SUB_MASK            = 0x00FFFFFFUL,
 
