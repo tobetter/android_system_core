640854aefe8fa095078bafc21e4de5f918aa01bd
Weiguang Ruan <Weiguang.ruan@amlogic.com>
2016-01-26 14:55:51 +0800

    PD #119211 bt: add support for AP6269/AP6242
    Change-Id: I86e00e9261e4c5040e5d1d29f633926e14d7d73e

--- init/devices.cpp
+++ init/devices.cpp
@@ -668,8 +668,8 @@ static void handle_generic_device_event(struct uevent *uevent)
         if (!assemble_devpath(devpath, subsystem->dirname, devname))
             return;
         mkdir_recursive_for_devpath(devpath);
-    } else if (!strncmp(uevent->subsystem, "usb", 3)) {
-         if (!strcmp(uevent->subsystem, "usb")) {
+    } else if (!strncmp(uevent->subsystem, "usb", 3) || !strncmp(uevent->subsystem, "usbmisc", 7)) {
+         if (!strcmp(uevent->subsystem, "usb") || !strcmp(uevent->subsystem, "usbmisc")) {
             if (uevent->device_name) {
                 if (!assemble_devpath(devpath, "/dev", uevent->device_name))
                     return;
